08f2074b1a3310dc6b163f8ea42b82562b15a4c5
EdPrys <eduard.prystupa@gmail.com>
2023-06-16 15:55:26 +0300

    Add:Function currency logic

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -4,31 +4,36 @@ import Select from "../forms/Select/Select";
 import { currency } from "../../utils/currency";
 import styles from "./Body.module.scss";
 
+const OPTIONS_SELECTED = ["USD", "UAH", "EUR"];
+
 const Body: React.FC = () => {
   const [inputValue1, setInputValue1] = useState("");
   const [inputValue2, setInputValue2] = useState("");
-  const [selectedOption1, setSelectedOption1] = useState("");
-  const [selectedOption2, setSelectedOption2] = useState("");
+  const [selectedOption1, setSelectedOption1] = useState(OPTIONS_SELECTED[0]);
+  const [selectedOption2, setSelectedOption2] = useState(OPTIONS_SELECTED[0]);
 
-  const handleInputChange1 = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange1 = async (e: ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
-    const convertedValue = currency(inputValue, [
-      selectedOption1,
-      selectedOption2,
-    ]);
-    setInputValue1(convertedValue.toString()); // TODO: set converted value in - setInputValue1
-    setInputValue2(inputValue); // TODO: copy velue in setInputValue2
-    currency(inputValue, [selectedOption1, selectedOption2]);
+    const convertedValue = await currency({
+      input: inputValue,
+      selects: [selectedOption1, selectedOption2],
+    });
+    setInputValue2(convertedValue); // TODO: set converted value in - setInputValue1
+    setInputValue1(inputValue); // TODO: copy velue in setInputValue2
+    currency({
+      input: inputValue,
+      selects: [selectedOption1, selectedOption2],
+    });
   };
 
-  const handleInputChange2 = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange2 = async (e: ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
-    const convertedValue = currency(inputValue, [
-      selectedOption1,
-      selectedOption2,
-    ]);
-    setInputValue2(convertedValue.toString()); // TODO: set converted value in - setInputValue2
-    setInputValue1(inputValue); // TODO: copy velue in setInputValue1
+    const convertedValue = await currency({
+      input: inputValue,
+      selects: [selectedOption1, selectedOption2],
+    });
+    setInputValue1(convertedValue); // TODO: set converted value in - setInputValue2
+    setInputValue2(inputValue); // TODO: copy velue in setInputValue1
   };
 
   const handleSelectChange1 = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -54,7 +59,7 @@ const Body: React.FC = () => {
       <Select
         value={selectedOption1}
         onChange={handleSelectChange1}
-        options={["USD", "UAH", "EUR"]}
+        options={OPTIONS_SELECTED}
       />
       <Input
         name="input2"
@@ -65,7 +70,7 @@ const Body: React.FC = () => {
       <Select
         value={selectedOption2}
         onChange={handleSelectChange2}
-        options={["USD", "UAH", "EUR"]}
+        options={OPTIONS_SELECTED}
       />
     </div>
   );
--- src/utils/currency.tsx
+++ src/utils/currency.tsx
@@ -1,11 +1,53 @@
 const API =
   "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json";
 
-export const currency = (input: string, selects: string[]): number => {
+type Props = {
+  input: string;
+  selects: any;
+};
+export const currency = async ({ input, selects }: Props): Promise<string> => {
   const [first, second] = selects;
   // convertation
+  if (input === "") {
+    return "";
+  } else if (first === second) {
+    return input;
+  } else {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
 
+      const euroRate = data.find((item: any) => item.cc === "EUR")?.rate;
+      const usdRate = data.find((item: any) => item.cc === "USD")?.rate;
+
+      if (first === "USD" && second === "UAH") {
+        const res = parseFloat(input) * usdRate; //TODO:USD -> UAH
+        return res.toFixed(2).toString();
+      } else if (first === "USD" && second === "EUR") {
+        const res = (parseFloat(input) * usdRate) / euroRate; //TODO:USD -> EUR
+        return res.toFixed(2).toString();
+      } else if (first === "EUR" && second === "UAH") {
+        const res = parseFloat(input) * euroRate; //TODO:EUR -> UAH
+        return res.toFixed(2).toString();
+      } else if (first === "EUR" && second === "USD") {
+        const res = (parseFloat(input) * euroRate) / usdRate; //TODO:EUR -> USD
+        return res.toFixed(2).toString();
+      } else if (first === "UAH" && second === "USD") {
+        const res = parseFloat(input) / usdRate; //TODO:UAH -> USD
+        return res.toFixed(2).toString();
+      } else if (first === "UAH" && second === "EUR") {
+        const res = parseFloat(input) / euroRate; //TODO:UAH -> EUR
+        return res.toFixed(2).toString();
+      } else {
+        throw new Error("Failed to retrieve exchange rates");
+      }
+    } catch (error) {
+      console.error("Currency conversion error:", error);
+      throw error;
+    }
+  }
+  // input === ""?"":
   const convertedValue = parseFloat(input);
   // console.log(selects[0] + " " + selects[1]);
-  return parseFloat(convertedValue.toFixed(2));
+  return parseFloat(convertedValue.toFixed(2)).toString();
 };
